af269aac28b06eb373fcd46a98d40da91f5edfb6
InkerBot <im@inker.bot>
2024-10-31 23:37:37 +0800

    bump version to 1.0.14-SNAPSHOT

--- ankh-invoke-gradle-plugin/build.gradle.kts
+++ ankh-invoke-gradle-plugin/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "org.inksnow"
-version = "1.0.13-SNAPSHOT"
+version = "1.0.14-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -27,7 +27,7 @@ gradlePlugin {
 }
 
 dependencies {
-    implementation("org.inksnow:ankh-invoke-mapping:1.0.13-SNAPSHOT")
+    implementation("org.inksnow:ankh-invoke-mapping:1.0.14-SNAPSHOT")
 
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
