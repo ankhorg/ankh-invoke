4d3164706d72cf0735deef55d5cbaa6aaa15dd08
Neige <2468629609@qq.com>
2024-11-29 22:53:41 +0800

    适配1.21.3

--- ankh-invoke-bukkit/src/main/java/org/inksnow/ankhinvoke/bukkit/util/CraftBukkitVersion.java
+++ ankh-invoke-bukkit/src/main/java/org/inksnow/ankhinvoke/bukkit/util/CraftBukkitVersion.java
@@ -26,7 +26,8 @@ public enum CraftBukkitVersion {
   v1_20_R2,
   v1_20_R3,
   v1_20_R4,
-  v1_21_R1;
+  v1_21_R1,
+  v1_21_R2;
 
   private static final @NotNull Logger logger = LoggerFactory.getLogger(CraftBukkitVersion.class);
   private static final @NotNull CraftBukkitVersion CURRENT = createCurrent();
--- ankh-invoke-gradle-plugin/build.gradle.kts
+++ ankh-invoke-gradle-plugin/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "org.inksnow"
-version = "1.0.16-SNAPSHOT"
+version = "1.0.17-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -27,7 +27,7 @@ gradlePlugin {
 }
 
 dependencies {
-    implementation("org.inksnow:ankh-invoke-mapping:1.0.16-SNAPSHOT")
+    implementation("org.inksnow:ankh-invoke-mapping:1.0.17-SNAPSHOT")
 
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
